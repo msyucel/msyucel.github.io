7d78b2f112dd069095a71ee569164e0f9ae66eee
Selman Yucel <mehmetselmanyucel@hotmail.com>
2026-07-01 18:22:39 +0300

    resume updated

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -113,12 +113,12 @@ $(document).ready(function(){
 
     // 5. welcome animation support
 
-        $(window).load(function(){
+        $(window).on('load', function(){
         	$(".header-text h2,.header-text p").removeClass("animated fadeInUp").css({'opacity':'0'});
             $(".header-text a").removeClass("animated fadeInDown").css({'opacity':'0'});
         });
 
-        $(window).load(function(){
+        $(window).on('load', function(){
         	$(".header-text h2,.header-text p").addClass("animated fadeInUp").css({'opacity':'0'});
             $(".header-text a").addClass("animated fadeInDown").css({'opacity':'0'});
         });
